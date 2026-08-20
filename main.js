23c5c9dbd8a52300502b1aa4c4e2dd1236779b34
Rinabelle <rinabellebandala51@gmail.com>
2026-08-20 10:53:49 +0800

    4 day for html and css

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("cart-btn").addEventListener("click", function() {
+    window.location.href = "storage.html";
+  });
+});
